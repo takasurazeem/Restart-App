505f1b31b0b5388f92773d90a2dc268938dd308d
Takasur Azeem <Takasur.Azeem1@ibm.com>
2022-09-21 16:18:23 -0400

    Initial Commit

---
+++ Restart-App/Restart_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Restart_AppApp.swift
+//  Restart-App
+//
+//  Created by Takasur Azeem on 21/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct Restart_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
