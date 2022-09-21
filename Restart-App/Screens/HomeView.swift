14ee5f7312c342c895fe3943f62c3b53e9287f07
Takasur Azeem <takasurazeem@gmail.com>
2022-09-21 16:52:18 -0400

    Basic functionality

---
+++ Restart-App/Screens/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  Restart-App
+//
+//  Created by Takasur Azeem on 21/09/2022.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Home")
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
---
+++ Restart-App/Screens/OnboardingView.swift
@@ -0,0 +1,20 @@
+//
+//  OnboardingView.swift
+//  Restart-App
+//
+//  Created by Takasur Azeem on 21/09/2022.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    var body: some View {
+        Text("Onboarding!")
+    }
+}
+
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView()
+    }
+}
