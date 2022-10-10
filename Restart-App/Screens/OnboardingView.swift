91e2125519724d6991cab7c64cc8aa81701a9b11
Takasur Azeem <takasurazeem@gmail.com>
2022-10-10 16:39:50 +0500

    Finishes onboarding screen
    Signed-off-by: Takasur Azeem <takasurazeem@gmail.com>

--- Restart-App/Screens/OnboardingView.swift
+++ Restart-App/Screens/OnboardingView.swift
@@ -56,6 +56,59 @@ struct OnboardingView: View {
                 Spacer()
                 // MARK: - Footer
                 
+                ZStack {
+                    // PARTS OF CUSTOM BUTTON
+                    
+                    // 1. BACKGROUND (STATIC)
+                    
+                    Capsule()
+                        .fill(.white.opacity(0.2))
+                    
+                    Capsule()
+                        .fill(.white.opacity(0.2))
+                        .padding(8)
+                    
+                    // 2. CALL-TO-ACTION (STATIC)
+                    
+                    Text("Get Started")
+                        .font(.system(.title3, design: .rounded, weight: .bold))
+                        .foregroundColor(.white)
+                        .offset(x: 20)
+                    
+                    // 3. CAPSULE (DYNAMIC WIDTH)
+                    
+                    HStack {
+                        Capsule()
+                            .fill(Color("ColorRed"))
+                            .frame(width: 80)
+                        
+                        Spacer()
+                    }
+                    
+                    // 4. CIRCLE (DRAGGABLE)
+                    
+                    HStack {
+                        ZStack {
+                            Circle()
+                                .fill(Color("ColorRed"))
+                            
+                            Circle()
+                                .fill(.black.opacity(0.15))
+                                .padding(8)
+                            Image(systemName: "chevron.right.2")
+                                .font(.system(size: 24, weight: .bold))
+                        }
+                        .foregroundColor(.white)
+                        .frame(width: 80, height: 80, alignment: .center)
+                        .onTapGesture {
+                            isOnboardingViewActive = false
+                        }
+                        
+                        Spacer()
+                    } //: HStack
+                } //: Footer
+                .frame(height: 80, alignment: .center)
+                .padding()
             } //: ZStack
         } //: VStack
     }
